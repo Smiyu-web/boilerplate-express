f6f45816426aa430881f9b26c5e259f7964adc32
Miu <miyuuu0728@gmail.com>
2021-05-27 12:06:07 -0700

    q3

--- myApp.js
+++ myApp.js
@@ -5,4 +5,9 @@ console.log("Hello World");
 app.get("/", (req, res) => {
   res.send("Hello Express");
 });
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
 module.exports = app;
