1ff6471c1892eb59bdda72cbf8063699a2d75fb7
Miu <miyuuu0728@gmail.com>
2021-05-27 18:52:16 -0700

    q11

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 require("dotenv").config();
+var bodyParser = require("body-parser");
 
 // console.log("Hello World");
 
@@ -59,15 +60,18 @@ require("dotenv").config();
 //   });
 // });
 
-app.get("/name", function (req, res) {
-  var firstName = req.query.first;
-  var lastName = req.query.last;
-  // OR you can destructure and rename the keys
-  var { first: firstName, last: lastName } = req.query;
-  // Use template literals to form a formatted string
-  res.json({
-    name: `${firstName} ${lastName}`,
-  });
-});
+// app.get("/name", function (req, res) {
+//   var firstName = req.query.first;
+//   var lastName = req.query.last;
+//   // OR you can destructure and rename the keys
+//   var { first: firstName, last: lastName } = req.query;
+//   // Use template literals to form a formatted string
+//   res.json({
+//     name: `${firstName} ${lastName}`,
+//   });
+// });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 module.exports = app;
